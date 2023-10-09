818bdf711868f8738afe7ee87624d50dbc17cc13
Marnien Cueba <marniencueba94@gmail.com>
2023-10-10 00:33:40 +0800

    update: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-2xl font-bold'>My Next JS Application!</h1>
-    </main>
-  )
+    <div className="flex-1 h-auto space-y-4">
+      <div className="flex-1 flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Home</h1>
+      </div>
+      <div>
+        Officia qui enim duis duis. Commodo velit dolore anim cupidatat Lorem
+        nulla ullamco nostrud ea occaecat et. Minim consectetur aute ad pariatur
+        cillum. Incididunt exercitation in deserunt officia deserunt velit dolor
+        duis deserunt aliquip. Dolore excepteur nisi cillum duis ut. Nostrud
+        exercitation incididunt ullamco laboris proident ex ut non minim minim.
+        In consectetur laborum amet qui elit exercitation Lorem aliqua ut nisi
+        nostrud et sint. Do cillum ipsum consequat duis eu consequat dolor.
+        Veniam incididunt fugiat adipisicing nostrud in pariatur occaecat
+        occaecat ex. Proident qui dolore non aliquip ad laboris officia est
+        incididunt sunt voluptate et. Sunt reprehenderit pariatur non eu mollit
+        eu ex dolore Lorem ex excepteur. Elit nulla non et dolore. Cillum minim
+        duis in quis velit nostrud excepteur pariatur occaecat laboris nostrud.
+        Elit ex culpa dolor laboris irure et laborum qui mollit. Laborum minim
+        pariatur tempor reprehenderit eiusmod aute officia ut occaecat nostrud
+        esse non enim. Laborum consequat eiusmod commodo voluptate ad veniam
+        laboris reprehenderit sint dolor cillum. Labore commodo reprehenderit
+        irure anim cupidatat voluptate consectetur. Cillum deserunt non sit
+        consectetur reprehenderit Lorem velit. Culpa deserunt amet ea Lorem.
+        Consequat et non eiusmod consectetur amet id non. Commodo irure amet
+        nostrud cupidatat in sunt eiusmod voluptate ex et nostrud. Sint Lorem
+        elit adipisicing officia cupidatat tempor sit ea cupidatat.
+      </div>
+    </div>
+  );
 }
